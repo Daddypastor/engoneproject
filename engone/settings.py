c82f6b500db2d6c806a58c01c4bffa7b28d8f2e1
emmaexcellent030@gmail.com <emmaexcellent030@gmail.com>
2022-08-27 05:46:30 +0100

    engone stil in process

--- engone/settings.py
+++ engone/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'engone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
